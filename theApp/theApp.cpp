338eacac4e27d6fa4cc000853a42ba1799938d0b
loumcnamee <mcnamee.lou@gmail.com>
2025-02-08 11:42:43 -0500

    add simple state machine using modern C++ variants

--- theApp/theApp.cpp
+++ theApp/theApp.cpp
@@ -1,45 +1,136 @@
-// LibraryApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <string>
 #include <iostream>
-#include <vector>
+#include <variant>
+#include <optional>
+#include <cstdint>
 
-#include "Inventory.h"
-#include "date.h"
+using namespace std;
+/* --------------------------------- Events ------------------------------------------ */
+struct EventStart {  };
+struct EventTooHot { };
+struct EventTooCold { };
+struct EventStop { };
 
-int main()
-{
-    std::cout << "Starting Inventory Management Application..." << std::endl; 
 
-    Inventory& myInventory = Inventory::getInstance();
+using Event = variant<EventStart, EventTooHot, EventTooCold, EventStop>;
+/* ------------------------------------------------------------------------------------- */
 
-    std::cout << "Library has " << myInventory.size() << "items " << std::endl;
 
-    ItemDate date1 = ItemDate(12,21,2021);
+/* --------------------------------- States ------------------------------------------ */
+struct IState {
+    virtual ~IState() = default;
+    float temp_ = 0.0;
+    float power_ = 0.0;
 
-    std::cout << "Hello, World\n";
+};
+struct Off : public IState { };
+struct Idle : public IState { };
+struct Heating : public IState { };
+struct Cooling : public IState{ };
 
-   std::vector<int> V{1, 2, 3, 4, 5};
-   int a = 8;
+using State = variant<Off, Idle, Heating, Cooling>;
+/* ------------------------------------------------------------------------------------- */
 
-   for(auto &e : V) {
-      std::cout << e << ' ';
-   }
-   std::cout << '\n';
 
-   std::cout << "a = " << a << '\n';
+/* ------------------------------- Transitions ---------------------------------------- */
+struct Transitions {
+    optional<State> operator()(Off &, const EventStart &e) {
+        cout << "Off -> Idle" << endl;
+        return Idle{};
+    }
 
-    std::cout << "Closing Inventory Management Application" << std::endl;
+    optional<State> operator()(Idle &, const EventTooHot &) {
+        cout << "Idle -> Cooling" << endl;
+        return Cooling{};
+    }
 
+    optional<State> operator()(Idle &, const EventTooCold &) {
+        cout << "Idle -> Heating" << endl;
+        return Heating{};
+    }
+    optional<State> operator()(Heating &s, const EventTooCold &) {
+        cout << "Heating -> Heating" << endl;
+        return Heating{};
+    }
+
+    optional<State> operator()(Cooling &s, const EventTooHot &) {
+        cout << "Cooling -> Cooling" << endl;
+        return Cooling{};
+    }
+
+    optional<State> operator()(Cooling &, const EventStop &) {
+        cout << "Cooling -> Idle" << endl;
+        return Idle{};
+    }
+
+    optional<State> operator()(Heating &, const EventStop &) {
+        cout << "Heating -> Idle" << endl;
+        return Idle{};
+    }
+    
+    optional<State> operator()(Idle &, const EventStop &) {
+        cout << "Idle -> Idle" << endl;
+        return Idle{};
+    }
+
+    template <typename State_t, typename Event_t>
+    optional<State> operator()(State_t &, const Event_t &) const {
+        cout << "Unkown" << endl;
+        return Idle{};
+    }
+};
+
+/* ------------------------------------------------------------------------------------- */
+template <typename StateVariant, typename EventVariant, typename Transitions>
+class Controller {
+    StateVariant m_curr_state = Off{};
+
+    void dispatch(const EventVariant &Event)
+    {
+        optional<StateVariant> new_state = visit(Transitions{}, m_curr_state, Event);
+        if (new_state)
+            m_curr_state = *move(new_state);
+    }
+
+    public: 
+        template <typename... Events>
+        void process(Events... e) { (dispatch(e), ...); }
+};
+
+int main() {
+    float temperture = 0.0;
+    float setting = 10.0;
+    Controller<State, Event, Transitions> ctrl;
+    
+    ctrl.process(EventStart{});
+    
+    
+    for (int i = 0; i < 100; i++) {
+    
+        if (temperture == setting ) {
+            ctrl.process(EventStop{});
+        }
+        if (temperture > setting) {
+            ctrl.process(EventTooHot{});
+            temperture += 1.0f;
+        }
+        if (temperture < setting) {
+            ctrl.process(EventTooCold{});
+            temperture += 1.0f;
+        }
+
+        std::cout << "Temp: " << temperture << std::endl;
+    }
+
+                      //      EventStarted{},
+                      //      EventTimeout{},
+                      //      EventConnected{},
+                      //      EventDisconnect{});
+    return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+/*
+Idle -> Connect
+Connecting -> Timeout
+Connecting -> Connected
+Connected -> Disconnect
+*/
